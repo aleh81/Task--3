1703a7c817e935a8c8412b7f498721b0cbef5ec5
Oleg <User@DESKTOP-2L28T8V>
2018-04-18 18:56:00 +0300

    a little estetic commit

--- Task3/Task3.BLL/Services/TimeCounter.cs
+++ Task3/Task3.BLL/Services/TimeCounter.cs
@@ -12,9 +12,11 @@ namespace Task3.BLL.Services
 		public static int Counter(Func<int, int, int> method, int a, int b, out long ticks)
 		{
 			var sWatch = Stopwatch.StartNew();
+
 			var result = method(a, b);
 
 			sWatch.Stop();
+
 			ticks = sWatch.ElapsedTicks;
 
 			return result;
@@ -23,9 +25,11 @@ namespace Task3.BLL.Services
 		public static int Counter(Func<int, int, int, int, int> method, int a, int b, int c, int d, out long ticks)
 		{
 			var sWatch = Stopwatch.StartNew();
+
 			var result = method(a, b, c, d);
 
 			sWatch.Stop();
+
 			ticks = sWatch.ElapsedTicks;
 
 			return result;
@@ -34,9 +38,11 @@ namespace Task3.BLL.Services
 		public static int Counter(Func<int[], int> method, int[] arguments, out long ticks)
 		{
 			var sWatch = Stopwatch.StartNew();
+
 			var result = method(arguments);
 
 			sWatch.Stop();
+
 			ticks = sWatch.ElapsedTicks;
 
 			return result;
